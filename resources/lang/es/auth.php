37a6dde57a1ec91e1de90bfaa4e53eb490a16b8b
garoga11 <a6519150007@utch.edu.mx>
2021-08-31 16:55:54 -0600

    creating a multilingual page

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
+    'throttle' => 'Demasiados intentos de acceso. Por favor inténtelo de nuevo en :seconds segundos.',
+
+    //titles
+    'title' => 'Tutorial de Laravel',
+    'doc_title' =>'Documentación',
+    'laracast' => 'Laracasts',
+    'news_title' => 'Noticias de Laravel',
+    'vibrant_title' => 'Ecosistema vibrante',
+
+    //text
+    'documentation_text' => 'Laravel tiene una maravillosa y completa documentación que cubre cada aspecto del framework. Tanto si eres nuevo en el framework como si tienes experiencia previa con Laravel, te recomendamos que leas toda la documentación de principio a fin.',
+    'laracast_text' => 'Laracasts ofrece miles de videotutoriales sobre el desarrollo de Laravel, PHP y JavaScript. Compruébalos, compruébalo por ti mismo, y sube de nivel masivamente tus habilidades de desarrollo en el proceso.',
+    'laravelNews_text' => 'Laravel News es un portal impulsado por la comunidad y un boletín que agrega todas las noticias más recientes e importantes en el ecosistema de Laravel, incluyendo nuevos lanzamientos de paquetes y tutoriales.',
+    'vibrantEcosystem_text' => 'La robusta biblioteca de herramientas y bibliotecas de Laravel, como Forge, Vapor, Nova y Envoyer, le ayudarán a llevar sus proyectos al siguiente nivel. Combínalas con potentes bibliotecas de código abierto como Cashier, Dusk, Echo, Horizon, Sanctum y Telescope, entre otras.',
+
+    //icons
+    'shop' => 'tienda',
+    'sponsor' => 'donaciones',
+
+];
